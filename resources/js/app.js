7ebbeced5f320e7e0d7edfbca96af54d7458ac22
uendel.couto <uendel.couto@gmail.com>
2021-04-17 15:54:34 -0300

    adiciona vuetify e vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,17 @@
 import Vue from 'vue';
 import router from './router.js';
 import App from './components/App';
+import vuetify from './vuetify';
+import store from './store';
 
 require('./bootstrap');
 
 const app = new Vue({
     el: '#app',
+    vuetify,
     components: {
         App,
     },
+    store,
     router,
 });
