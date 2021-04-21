74b3dff14f787d9f465274634900ed2119307211
uendel.couto <uendel.couto@gmail.com>
2021-04-21 18:35:17 -0300

    adiciona navbar no app para testes

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
+    <!-- <nav-bar></nav-bar> -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import NavBar from './NavBar.vue'
 export default {
+  components: { NavBar },
     name: 'App',
 }
 </script>
