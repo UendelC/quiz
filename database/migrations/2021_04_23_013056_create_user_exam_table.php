de2d3c00a22ea8c946fc48f044bbfaf5dff6c9d0
uendel.couto <uendel.couto@gmail.com>
2021-04-22 22:39:07 -0300

    cria migrations

---
+++ database/migrations/2021_04_23_013056_create_user_exam_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserExamTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_exam', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->integer('exam_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade');
+            $table->foreign('exam_id')->references('id')->on('exams')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_exam');
+    }
+}
