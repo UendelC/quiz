0a42f4d38904f5966dd7c97baf97acd9a8283f12
uendel.couto <uendel.couto@gmail.com>
2021-04-23 23:48:58 -0300

    corrige migration

--- database/migrations/2021_04_12_225259_create_questions_table.php
+++ database/migrations/2021_04_12_225259_create_questions_table.php
@@ -16,6 +16,7 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('exam_id');
             $table->foreign('category_id')
                 ->references('id')
                 ->on('categories');
