d8d388f2b8a033f57d3b5e29b049ed7405fabfe9
uendel.couto <uendel.couto@gmail.com>
2021-04-30 20:34:12 -0300

    refatora padrão de projeto vue

---
+++ resources/js/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <example-component>
+  </example-component>
+</template>
+
+<script>
+import ExampleComponent from '../components/ExampleComponent';
+export default {
+  components: {
+    ExampleComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
