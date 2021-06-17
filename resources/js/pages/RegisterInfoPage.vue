57f7294d3e8facface037573bbfc5eedb3a22b0c
uendel.couto <uendel.couto@gmail.com>
2021-06-16 23:37:24 -0300

    adiciona select de categoria

--- resources/js/pages/RegisterInfoPage.vue
+++ resources/js/pages/RegisterInfoPage.vue
@@ -8,13 +8,18 @@
         label="Categoria:"
         label-for="input-1"
       >
-        <b-form-input
+      <b-form-select
+        v-model="form.category"
+        :options="options"
+      >
+      </b-form-select>
+        <!-- <b-form-input
           id="input-1"
           v-model="form.category"
           type="select"
           placeholder="Categoria"
           required
-        ></b-form-input>
+        ></b-form-input> -->
       </b-form-group>
 
       <b-form-group id="input-group-2" label="Enunciado da Questão:" label-for="input-2">
@@ -67,6 +72,13 @@ import NavBar from '../components/NavBar';
         },
         show: true,
         letterIndex: 0,
+        options: [
+          { value: null, text: 'Please select an option' },
+          { value: 'a', text: 'This is First option' },
+          { value: 'b', text: 'Selected Option' },
+          { value: { C: '3PO' }, text: 'This is an option with object value' },
+          { value: 'd', text: 'This one is disabled', disabled: true }
+        ],
       }
     },
 
