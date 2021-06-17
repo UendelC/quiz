354cef85f9040e45abb4f4ec5dcfa2430fe87ac7
uendel.couto <uendel.couto@gmail.com>
2021-06-16 23:34:32 -0300

    adiciona método para retornar categorias

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = Category::all();
+
+        return  new CategoryResource($categories);
+    }
 }
