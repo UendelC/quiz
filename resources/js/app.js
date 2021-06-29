962244f7ff3af7b4a873894bc5d389b6cd3605b4
uendel.couto <uendel.couto@gmail.com>
2021-06-28 23:14:27 -0300

    adiciona cuida ao projeto

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,11 @@ import router from './router.js';
 import App from './components/App';
 import bootstrap from './bootstrap-components';
 import store from './store';
+import Cuida from '@sysvale/cuida';
 
 require('./bootstrap');
+
+Vue.use(Cuida);
 
 const app = new Vue({
     el: '#app',
