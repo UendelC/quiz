9274ad3644ec5dfc67213d8a2de70ff1eb36cf18
uendel.couto <uendel.couto@gmail.com>
2021-07-15 14:01:54 -0300

    corrige relacionamento

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -9,6 +9,10 @@ class Exam extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'category_id',
+    ];
+
     public function users()
     {
         return $this->belongsToMany(User::class);
