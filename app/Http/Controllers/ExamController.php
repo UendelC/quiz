cd430c57f29a5b84150c537745bfb76f392ef4ed
uendel.couto <uendel.couto@gmail.com>
2021-07-25 22:39:10 -0300

    reverte teste

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -120,9 +120,9 @@ class ExamController extends Controller
             'date' => $exam->created_at->format('d/m/Y'),
         ];
 
-        // Sheets::spreadsheet('1SRGZH4PaHn-w52GI1ZwdTCJsjVLundz4rPN4A66k4Yg')
-        //     ->sheet('Página3')
-        //     ->append($data);
+        Sheets::spreadsheet('1SRGZH4PaHn-w52GI1ZwdTCJsjVLundz4rPN4A66k4Yg')
+            ->sheet('Página3')
+            ->append($data);
 
         return response()->json(
             [
