28fb8113a712cb4e353622c782434a35fcb490af
uendel.couto <uendel.couto@gmail.com>
2021-08-11 17:02:59 -0300

    adiciona tabela inicial do crud de avaliações

--- resources/js/pages/ExamManagement.vue
+++ resources/js/pages/ExamManagement.vue
@@ -1,10 +1,33 @@
 <template>
-  
+  <div>
+    <nav-bar></nav-bar>
+    <div class="container">
+        <b-table
+          striped
+          hover
+          :items="[]"
+          :fields="[]"
+          bordered
+          sticky-header
+          :busy="false"
+          show-empty
+          empty-text="Não há avaliações"
+        >
+          <template #empty="scope">
+            <h4>{{ scope.emptyText }}</h4>
+          </template>
+        </b-table>
+      </div>
+  </div>
 </template>
 
 <script>
-export default {
+import NavBar from '../components/NavBar';
 
+export default {
+  components: {
+    NavBar,
+  },
 }
 </script>
 
