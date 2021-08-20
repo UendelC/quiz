1591f4fa0fce07ee10416af9f40752f56377ca12
uendel.couto <uendel.couto@gmail.com>
2021-08-20 17:02:25 -0300

    adiciona teste

---
+++ tests/Feature/CategoryControllerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use App\Models\Exam;
+use App\Models\Subject;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class CategoryControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testATeacherCanGetTheirCategories()
+    {
+        $this->withoutExceptionHandling();
+        $teacher = User::factory()->teacher()->create();
+
+        $subject = Subject::factory()->create(['teacher_id' => $teacher->id]);
+        $category_from_teacher = Category::factory()->create();
+        $another_category = Category::factory()->create();
+
+        Exam::factory(2)->create(
+            [
+                'subject_id' => $subject->id,
+                'category_id' => $category_from_teacher->id,
+            ]
+        );
+
+        Sanctum::actingAs($teacher);
+
+        $this->json('GET', '/api/categories-from-teacher')
+            ->assertStatus(200)
+            ->assertJson(
+                [
+                    'categories' => [
+                        [
+                            'id' => $category_from_teacher->id,
+                            'name' => $category_from_teacher->name,
+                        ],
+                    ],
+                ]
+            )
+            ->assertJsonMissing(
+                [
+                    'categories' => [
+                        [
+                            'id' => $another_category->id,
+                            'name' => $another_category->name,
+                        ],
+                    ],
+                ]
+            );
+    }
+}
