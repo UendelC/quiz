b4e22d5f08f6d4839a9189699a7fb07c2c303781
uendel.couto <uendel.couto@gmail.com>
2021-08-21 12:07:14 -0300

    corrige relatório

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -8,18 +8,30 @@ class ReportController extends Controller
 {
     public function process(Request $request)
     {
+        dump('aqui');
         $teacher = auth()->user();
         $teacher_query = $teacher->lecture->exams();
 
         if ($request->has('participants')) {
             dump('com participantes');
             $teacher_query = $teacher_query
-                ->whereHas(
-                    'users',
-                    function ($user) use ($request) {
-                        $user->whereIn('id', $request->participants);
+                ->map(
+                    function ($exam) use ($request) {
+                        $exam
+                            ->users()
+                            ->whereIn('id', $request->participants);
+                        
+                        return $exam;
                     }
                 );
+
+            dd($teacher_query);
+                // ->whereHas(
+                //     'users',
+                //     function ($user) use ($request) {
+                //         $user->whereIn('id', $request->participants);
+                //     }
+                // );
         }
 
         if ($request->has('categories')) {
@@ -45,7 +57,48 @@ class ReportController extends Controller
                 ->whereDate('date', $request->date); // between
         }
 
-        dd($teacher_query->get()->toArray());
-        return $teacher_query->get();
+        $report = $teacher_query
+            ->with('users')
+            ->get()
+            ->map(
+                function ($exam) {
+                    $score = $exam->users->map(
+                        function ($user) {
+                            return $user->pivot->score;
+                        }
+                    );
+                    $exam->mean_score = $score->avg();
+                    $exam->scores = $score->toArray();
+                    $exam->standard_deviation = $this->standardDeviation($score->toArray());
+                    return $exam;
+                }
+            )
+            ->toArray();
+
+        dd($report);
+
+        return response()->json(
+            [
+                'mean_score' => $report['mean_score'],
+                'standard_deviation' => $report['standard_deviation'],
+                'scores' => $report['scores'],
+            ]
+        );
+    }
+
+    private function standardDeviation(array $scores)
+    {
+        $mean = array_sum($scores) / count($scores);
+        $variance = array_sum(
+            array_map(
+                function ($x) use ($mean) {
+                    return pow($x - $mean, 2);
+                },
+                $scores
+            )
+        ) / count($scores);
+
+        return sqrt($variance);
+
     }
 }
