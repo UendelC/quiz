8b3f05814460362b92dbd92d860565113e00ab4b
uendel.couto <uendel.couto@gmail.com>
2021-08-20 22:08:45 -0300

    implementa reportController

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    public function process(Request $request)
+    {
+        if ($request->has('participants')) {
+
+        }
+
+        if ($request->has('categories')) {
+
+        }
+
+        if ($request->has('exams')) {
+
+        }
+
+        if ($request->has('date')) {
+
+        }
+    }
+}
