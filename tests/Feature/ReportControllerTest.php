08f06689a076d4599dc70fc1b6c0e66b113d4499
uendel.couto <uendel.couto@gmail.com>
2021-08-25 23:36:56 -0300

    adiciona testes que garantem funcionamento de reportController

--- tests/Feature/ReportControllerTest.php
+++ tests/Feature/ReportControllerTest.php
@@ -69,4 +69,195 @@ class ReportControllerTest extends TestCase
             );
 
     }
+
+    public function testUsersCanGetReportWithParticipantsFilter()
+    {
+        $this->withoutExceptionHandling();
+        $teacher = User::factory()->teacher()->create();
+        $participant = User::factory()->participant()->create();
+        $another_participant = User::factory()->participant()->create();
+
+        $subject = Subject::factory()->create(
+            ['teacher_id' => $teacher->id]
+        );
+
+        $category = Category::factory()->create();
+        $exam = Exam::factory()->create(
+            [
+                'subject_id' => $subject->id,
+                'category_id' => $category->id,
+            ]
+        );
+
+        $exam->users()->attach($participant->id, ['score' => 10]);
+        $exam->users()->attach($another_participant->id, ['score' => 8]);
+
+        $another_exam = Exam::factory()->create(
+            [
+                'subject_id' => $subject->id,
+                'category_id' => $category->id,
+            ]
+        );
+
+        $another_exam->users()->attach($participant->id, ['score' => 4]);
+        $another_exam->users()->attach($another_participant->id, ['score' => 0]);
+
+        Sanctum::actingAs($teacher);
+        $this->json('POST', 'api/report', ['participants' => [$participant->id]])
+            ->assertStatus(200)
+            ->assertJson(
+                [
+                    'mean_score' => 7.0,
+                    'standard_deviation' => 3.0,
+                    'scores' => [
+                        '10.0',
+                        '4.0',
+                    ],
+                ]
+            );
+    }
+
+    public function testUsersCanGetReportWithCategoryFilter()
+    {
+        $this->withoutExceptionHandling();
+        $teacher = User::factory()->teacher()->create();
+
+        $participant = User::factory()->participant()->create();
+        $another_participant = User::factory()->participant()->create();
+
+        $category = Category::factory()->create();
+        $another_category = Category::factory()->create();
+
+        $subject = Subject::factory()->create(
+            ['teacher_id' => $teacher->id]
+        );
+
+        $exam = Exam::factory()->create(
+            [
+                'subject_id' => $subject->id,
+                'category_id' => $category->id,
+            ]
+        );
+        $exam->users()->attach($participant->id, ['score' => 10]);
+        $exam->users()->attach($another_participant->id, ['score' => 8]);
+
+        $another_exam = Exam::factory()->create(
+            [
+                'subject_id' => $subject->id,
+                'category_id' => $another_category->id,
+            ]
+        );
+        $another_exam->users()->attach($participant->id, ['score' => 4]);
+        $another_exam->users()->attach($another_participant->id, ['score' => 0]);
+
+        Sanctum::actingAs($teacher);
+
+        $this->json('POST', 'api/report', ['categories' => [$category->id]])
+            ->assertStatus(200)
+            ->assertJson(
+                [
+                    'mean_score' => 9.0,
+                    'standard_deviation' => 1.0,
+                    'scores' => [
+                        '10.0',
+                        '8.0',
+                    ],
+                ]
+            );
+    }
+
+    public function testUsersCanGetReportWithExamsFilter()
+    {
+        $teacher = User::factory()->teacher()->create();
+
+        $participant = User::factory()->participant()->create();
+        $another_participant = User::factory()->participant()->create();
+
+        $subject = Subject::factory()->create(
+            ['teacher_id' => $teacher->id]
+        );
+
+        $category = Category::factory()->create();
+
+        $exam = Exam::factory()->create(
+            [
+                'subject_id' => $subject->id,
+                'category_id' => $category->id,
+            ]
+        );
+
+        $exam->users()->attach($participant->id, ['score' => 10]);
+        $exam->users()->attach($another_participant->id, ['score' => 8]);
+
+        $another_exam = Exam::factory()->create(
+            [
+                'subject_id' => $subject->id,
+                'category_id' => $category->id,
+            ]
+        );
+
+        $another_exam->users()->attach($participant->id, ['score' => 4]);
+        $another_exam->users()->attach($another_participant->id, ['score' => 0]);
+
+        Sanctum::actingAs($teacher);
+
+        $this->json('POST', 'api/report', ['exams' => [$exam->id]])
+            ->assertStatus(200)
+            ->assertJson(
+                [
+                    'mean_score' => 9.0,
+                    'standard_deviation' => 1.0,
+                    'scores' => [
+                        '10.0',
+                        '8.0',
+                    ],
+                ]
+            );
+    }
+
+    public function testUsersCanGetReportWithDateFilter()
+    {
+        $teacher = User::factory()->teacher()->create();
+
+        $participant = User::factory()->participant()->create();
+        $another_participant = User::factory()->participant()->create();
+
+        $date = Carbon::now()->subDays(2);
+        $another_date = Carbon::now()->subDays(1);
+
+        $subject = Subject::factory()->create(
+            ['teacher_id' => $teacher->id]
+        );
+
+        $category = Category::factory()->create();
+
+        $exam = Exam::factory()->create(
+            [
+                'subject_id' => $subject->id,
+                'category_id' => $category->id,
+            ]
+        );
+
+        $exam->users()->attach($participant->id, ['score' => 10, 'created_at' => $date]);
+        $exam->users()->attach($another_participant->id, ['score' => 8, 'created_at' => $date]);
+        $exam->users()->attach($participant->id, ['score' => 4, 'created_at' => $another_date]);
+        $exam->users()->attach($another_participant->id, ['score' => 0, 'created_at' => $another_date]);
+
+        Sanctum::actingAs($teacher);
+
+        $this->json('POST', 'api/report', ['date' => $date->toDateString()])
+            ->assertStatus(200)
+            ->assertJson(
+                [
+                    'mean_score' => 7.0,
+                    'standard_deviation' => 3.0,
+                    'scores' => [
+                        '10.0',
+                        '8.0',
+                        '4.0',
+                    ],
+                ]
+            );
+    }
+
 }
